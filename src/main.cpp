1a0e802b1e446d4ce18c21da2a4bbf919102714c
Rosmianto Aji Saputro <rosmianto.site@gmail.com>
2020-04-15 17:30:34 +0700

    Add main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,4 @@
-// 1-channel LoRa Gateway for ESP8266
-// Copyright (c) 2016-2020 Maarten Westenberg
-// Author: Maarten Westenberg (mw12554@hotmail.com)
-//
-// Based on work done by Thomas Telkamp for Raspberry PI 1-ch gateway and many others.
-//
-// All rights reserved. This program and the accompanying materials
-// are made available under the terms of the MIT License
-// which accompanies this distribution, and is available at
-// https://opensource.org/licenses/mit-license.php
-//
-// NO WARRANTY OF ANY KIND IS PROVIDED
-//
-// The protocols and specifications used for this 1ch gateway: 
-// 1. LoRA Specification version V1.0 and V1.1 for Gateway-Node communication
-//	
-// 2. Semtech Basic communication protocol between Lora gateway and server version 3.0.0
-//	https://github.com/Lora-net/packet_forwarder/blob/master/PROTOCOL.TXT
-//
-// Notes: 
-// - Once call hostbyname() to get IP for services, after that only use IP
-//	 addresses (too many gethost name makes the ESP unstable)
-// - Only call yield() in main stream (not for background NTP sync). 
-//
-// ----------------------------------------------------------------------------------------
+
 
 #if defined (ARDUINO_ARCH_ESP32) || defined(ESP32)
 #	define ESP32_ARCH 1
